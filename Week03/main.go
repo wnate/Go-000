b198d99b8f30182e30d8516f9f4c08796c701e16
nate <itgitor@gmail.com>
2020-12-10 09:40:54 +0800

    关服超时

--- Week03/main.go
+++ Week03/main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"github.com/wnate/Go-000/tree/main/Week03/app"
 	"github.com/wnate/Go-000/tree/main/Week03/graceful"
+	"log"
+	"time"
 )
 
 func main() {
 	graceful.AddShutDownHook(app.Start(), func() {
-		<-app.Stop()
+		select {
+		case <-app.Stop():
+			log.Printf("app stop success")
+		case <-time.After(30 * time.Second):
+			log.Printf("app stop timeout")
+		}
 	})
 }
